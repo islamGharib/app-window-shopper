19c7166c172fb342bba12e7eda6cf28e2bd16e66
Islam Gharib <islamgharib@Islams-MacBook-Pro.local>
2020-01-30 22:22:43 +0200

    creating a custom UITextField

---
+++ window-shopper/window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,27 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Islam Gharib on 1/30/20.
+//  Copyright © 2020 Gharib. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2481538955)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder{
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+
+}
